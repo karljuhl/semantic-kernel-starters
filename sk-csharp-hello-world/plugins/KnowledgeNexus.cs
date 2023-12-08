48624aee21c236a25e5a7dbd163f1f473bee164b
juhl.r <147093720+seb-AA@users.noreply.github.com>
2023-12-07 20:12:06 -0700

    Update KnowledgeNexus.cs

--- sk-csharp-hello-world/plugins/KnowledgeNexus.cs
+++ sk-csharp-hello-world/plugins/KnowledgeNexus.cs
@@ -1,157 +1,68 @@
-// SK plugin which calls on Custom QnA and Conversational Language Understanding for asynchronous data gathering in order to construct a response built in a context heavy environment 
-
-// SK Plugin
-
-name: KnowledgeNexus
-template: |
-{{#message role="system"}}
-  ## Instructions
-  Gather the data returned to form the best answer you reasonably can 
-
-  ## Rules
-  - Do not fabricate any information
-  - Other rules
-  {{/message}}
-
-{{#message role="user"}}
-{{user_query}}
-{{/message}}
-
-{{ IF CLU value returned: }}
-Here is some context of the user's input that can help you understand their query better: {CLU Intent, CLU Entities}
-{{/if}}
-
-{{ IF RAG context pulled: }}
-Here is some useful information that could be relevant to answer the user query: {RAG Context Chunks}
-{{/if}}
-
-{{ IF CQA answer returned: }}
-Here is an existing answer to the user's question, that you may use to help guide your response: {CQA Answer}
-{{/if}}
-
-template_format: handlebars
-description: A function that combines data outputs from RAG, CLU, and CQA resources to create the best possible answer to a users query
-input_variables:
-  - name: messages
-    type: ChatHistory
-    description: The history of the chat.
-    is_required: true
-output_variable:
-    type: string
-    description: The response from the assistant.
-execution_settings:
-  - model_id_pattern: ^gpt-4
-    temperature: 0.7
-  - model_id_pattern: ^gpt-3\.?5-turbo
-    temperature: 0.3
-
-
-
-
-
-        
-// CQA_KN.cs
-// Asynchoronous fetching of related answers from a CQA resource using azure sdk for .net
-        
 using Azure.Core;
 using Azure.AI.Language.QuestionAnswering;
-
-Uri endpoint = new Uri("https://myaccount.cognitiveservices.azure.com/");
-AzureKeyCredential credential = new AzureKeyCredential("{api-key}");
-
-QuestionAnsweringClient client = new QuestionAnsweringClient(endpoint, credential);
-
-string projectName = "{ProjectName}";
-string deploymentName = "{DeploymentName}";
-QuestionAnsweringProject project = new QuestionAnsweringProject(projectName, deploymentName);
-Response<AnswersResult> response = await client.GetAnswersAsync("How long should my Surface battery last?", project);
-
-foreach (KnowledgeBaseAnswer answer in response.Value.Answers)
-{
-    Console.WriteLine($"({answer.Confidence:P2}) {answer.Answer}");
-    Console.WriteLine($"Source: {answer.Source}");
-    Console.WriteLine();
-}
-
-
-
-
-
-
-
-// CLU_KN.cs
-// Asynchoronous fetching of related CLU entities and intents from the azure sdk for .net
-
-using Azure.Core;
-using Azure.Core.Serialization;
 using Azure.AI.Language.Conversations;
+using Microsoft.SemanticKernel;
+using System.ComponentModel;
+using System.Threading.Tasks;
+using System;
 
-Uri endpoint = new Uri("https://myaccount.cognitiveservices.azure.com");
-AzureKeyCredential credential = new AzureKeyCredential("{api-key}");
-
-ConversationAnalysisClient client = new ConversationAnalysisClient(endpoint, credential);
-
-string projectName = "Menu";
-string deploymentName = "production";
-
-var data = new
+public class KnowledgeNexusPlugin
 {
-    AnalysisInput = new
+    private readonly AzureKeyCredential credential;
+    private readonly Uri cqaEndpoint;
+    private readonly Uri cluEndpoint;
+    private readonly string cqaProjectName;
+    private readonly string cqaDeploymentName;
+    private readonly string cluProjectName;
+    private readonly string cluDeploymentName;
+    private readonly QuestionAnsweringClient cqaClient;
+    private readonly ConversationAnalysisClient cluClient;
+
+    public KnowledgeNexusPlugin()
     {
-        ConversationItem = new
-        {
-            Text = "Send an email to Carol about tomorrow's demo",
-            Id = "1",
-            ParticipantId = "1",
-        }
-    },
-    Parameters = new
+        credential = new AzureKeyCredential("{api-key}");
+        cqaEndpoint = new Uri("https://hckthn.cognitiveservices.azure.com/");
+        cluEndpoint = new Uri("https://hckthn.cognitiveservices.azure.com/");
+        cqaProjectName = "<YOUR CQA PROJECT NAME>";
+        cqaDeploymentName = "<YOUR CQA DEPLOYMENT NAME>";
+        cluProjectName = "<YOUR CLU PROJECT NAME>";
+        cluDeploymentName = "<YOUR CLU DEPLOYMENT NAME>";
+
+        cqaClient = new QuestionAnsweringClient(cqaEndpoint, credential);
+        cluClient = new ConversationAnalysisClient(cluEndpoint, credential);
+    }
+
+    [SKFunction]
+    [Description("Gathers responses from CQA and CLU services to build the best answer for a user's query.")]
+    [SKOutputDescription("The responses from CQA and CLU services.")]
+    public async Task<string> HandleRequestAsync(
+        [Description("The user's query to process.")] string userQuery)
     {
-        ProjectName = projectName,
-        DeploymentName = deploymentName,
+        // Asynchronously call CQA and CLU services
+        var cqaResponse = await GetCqaResponseAsync(userQuery);
+        var cluResponse = await GetCluResponseAsync(userQuery);
 
-        // Use Utf16CodeUnit for strings in .NET.
-        StringIndexType = "Utf16CodeUnit",
-    },
-    Kind = "Conversation",
-};
+        // Process responses
+        return ProcessResponses(cqaResponse, cluResponse);
+    }
 
-Response response = await client.AnalyzeConversationAsync(RequestContent.Create(data, JsonPropertyNames.CamelCase));
-
-dynamic conversationalTaskResult = response.Content.ToDynamicFromJson(JsonPropertyNames.CamelCase);
-dynamic conversationPrediction = conversationalTaskResult.Result.Prediction;
-
-Console.WriteLine($"Top intent: {conversationPrediction.TopIntent}");
-
-Console.WriteLine("Intents:");
-foreach (dynamic intent in conversationPrediction.Intents)
-{
-    Console.WriteLine($"Category: {intent.Category}");
-    Console.WriteLine($"Confidence: {intent.ConfidenceScore}");
-    Console.WriteLine();
-}
-
-Console.WriteLine("Entities:");
-foreach (dynamic entity in conversationPrediction.Entities)
-{
-    Console.WriteLine($"Category: {entity.Category}");
-    Console.WriteLine($"Text: {entity.Text}");
-    Console.WriteLine($"Offset: {entity.Offset}");
-    Console.WriteLine($"Length: {entity.Length}");
-    Console.WriteLine($"Confidence: {entity.ConfidenceScore}");
-    Console.WriteLine();
-
-    if (entity.Resolutions is not null)
+    private async Task<string> GetCqaResponseAsync(string query)
     {
-        foreach (dynamic resolution in entity.Resolutions)
-        {
-            if (resolution.ResolutionKind == "DateTimeResolution")
-            {
-                Console.WriteLine($"Datetime Sub Kind: {resolution.DateTimeSubKind}");
-                Console.WriteLine($"Timex: {resolution.Timex}");
-                Console.WriteLine($"Value: {resolution.Value}");
-                Console.WriteLine();
-            }
-        }
+        // Call CQA service and process response
+        QuestionAnsweringProject project = new QuestionAnsweringProject(cqaProjectName, cqaDeploymentName);
+        Response<AnswersResult> response = await cqaClient.GetAnswersAsync(query, project);
+        // Process and return the CQA response
+    }
+
+    private async Task<string> GetCluResponseAsync(string query)
+    {
+        // Call CLU service and process response
+        // Implement the logic to call CLU service with the query and process the response
+    }
+
+    private string ProcessResponses(string cqaResponse, string cluResponse)
+    {
+        // Combine insights from CQA and CLU
+        // Implement the logic to process and combine the responses from CQA and CLU services
     }
 }
